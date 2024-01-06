c6d74fcf3ab9d126b0054d393c7122bfe851ba3f
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2024-01-07 05:03:10 +0900

    Fix: 微修正

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -1,17 +1,24 @@
 class OauthController < ApplicationController
   def callback
     # binding.pry
+    host = '172.30.0.2'
     client = OAuth2::Client.new(
                                 '-J1fePKFRCDbAUme6FXzjeTgaAbIr0i3f8OV7ao8IPM',
                                 'GuADl4cSB3sCfSa1Abha313I1yBswP9UrBIzvrm_6c8',
-                                site: 'http://172.30.0.2:3003'
+                                site: "http://#{host}:3003",
+                                authorize_url: "http://#{host}:3003/oauth/authorize",
+                                token_url: "http://#{host}:3003/oauth/token",
                               )
-    client.auth_code.authorize_url(redirect_uri: 'http://localhost:3004/oauth/callback')
+    # 以下は認証URLが取得できる
+    # client.auth_code.authorize_url(redirect_uri: 'http://localhost:3004/oauth/callback')
     access = client.auth_code.get_token(
                                         params[:code],
                                         redirect_uri: 'http://localhost:3004/oauth/callback'
                                       )
-    # binding.pry
+    token = access.token
+    response = access.get('/api/v1/me', headers: {"Authorization" => "Bearer #{token}"})
+    body = JSON.parse(response.body)
+    binding.pry
 
   end
 end
